d3bba599c2f9481cddfe1d45cfe9fd97d9408405
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-10-24 20:59:58 +0900

    photon file set

---
+++ Fight_Cat/Assets/Scripts/Lancher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Lancher : MonoBehaviour
+{
+
+    #region private Serializable Fields
+
+    #endregion
+
+
+    #region Private Fields
+
+    /// <summary>
+    /// 클라이언트 버전 번호
+    /// </summary>
+    string gameVersion = "1";
+
+    #endregion
+
+
+
+    #region Public Methods
+
+    #endregion
+
+}
