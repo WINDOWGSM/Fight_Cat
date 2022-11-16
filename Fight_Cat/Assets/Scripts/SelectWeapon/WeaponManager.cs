830f19b48fac820127be3b18b44a72778c0acbd7
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-11-16 10:42:03 +0900

    무기 및 UI Prefab 변경

--- Fight_Cat/Assets/Scripts/SelectWeapon/WeaponManager.cs
+++ Fight_Cat/Assets/Scripts/SelectWeapon/WeaponManager.cs
@@ -8,7 +8,7 @@ public class WeaponManager : MonoBehaviour
     public enum WeaponType
     {
         Bow,    
-        Gun,
+        Bomb,
         Hammer,
         Rifle,
         Sword,
@@ -19,6 +19,8 @@ public class WeaponManager : MonoBehaviour
 
     public GameObject _selectWeaponPanel;
     [SerializeField] Button[] weaponSelectButtons;
+    [SerializeField] Image selectWeaponImage;
+    
     
 
     // Start is called before the first frame update
@@ -30,12 +32,12 @@ public class WeaponManager : MonoBehaviour
 
     void SetSelectButton()
     {
-        weaponSelectButtons[0].onClick.AddListener(() => SetWeapon(WeaponType.Bow));
-        weaponSelectButtons[1].onClick.AddListener(() => SetWeapon(WeaponType.Gun));
-        weaponSelectButtons[2].onClick.AddListener(() => SetWeapon(WeaponType.Hammer));
-        weaponSelectButtons[3].onClick.AddListener(() => SetWeapon(WeaponType.Rifle));
-        weaponSelectButtons[4].onClick.AddListener(() => SetWeapon(WeaponType.Sword));
-        weaponSelectButtons[5].onClick.AddListener(() => SetWeapon(WeaponType.Sycthe));
+        weaponSelectButtons[0].onClick.AddListener(() => SetWeapon(WeaponType.Bow, weaponSelectButtons[0].GetComponent<Image>()));
+        weaponSelectButtons[1].onClick.AddListener(() => SetWeapon(WeaponType.Bomb, weaponSelectButtons[1].GetComponent<Image>()));
+        weaponSelectButtons[2].onClick.AddListener(() => SetWeapon(WeaponType.Hammer, weaponSelectButtons[2].GetComponent<Image>()));
+        weaponSelectButtons[3].onClick.AddListener(() => SetWeapon(WeaponType.Rifle, weaponSelectButtons[3].GetComponent<Image>()));
+        weaponSelectButtons[4].onClick.AddListener(() => SetWeapon(WeaponType.Sword, weaponSelectButtons[4].GetComponent<Image>()));
+        weaponSelectButtons[5].onClick.AddListener(() => SetWeapon(WeaponType.Sycthe, weaponSelectButtons[5].GetComponent<Image>()));
         
     }
 
@@ -45,9 +47,11 @@ public class WeaponManager : MonoBehaviour
         
     }
 
-    private void SetWeapon(WeaponType _weapon)
+    private void SetWeapon(WeaponType _weapon, Image _image)
     {
         myWeapon = _weapon;
+        selectWeaponImage.sprite = _image.sprite;
+
     }
 
 }
