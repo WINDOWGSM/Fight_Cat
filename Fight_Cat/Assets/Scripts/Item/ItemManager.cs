c89effea68a1e9ec017a96196bc10b5928c79de6
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-11-21 23:29:11 +0900

    item Random Spawn

--- Fight_Cat/Assets/Scripts/Item/ItemManager.cs
+++ Fight_Cat/Assets/Scripts/Item/ItemManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] itemPrefabs;
+
+    [SerializeField] float spawnTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnItem());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    IEnumerator SpawnItem()
+    {
+        float timer = 0f;
+
+        while (true)
+        {
+
+            Vector3 rand_Vec = new Vector3(Random.Range(-15, 16), Random.Range(-10, 10),0);
+
+            Instantiate(itemPrefabs[Random.Range(0, itemPrefabs.Length)],rand_Vec,Quaternion.identity);
+
+
+            yield return new WaitForEndOfFrame();
+
+        }
     }
 }
