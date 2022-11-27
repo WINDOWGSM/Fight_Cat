7be9eb1a63d1e5d6f5444ac058d55a06f263a625
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-11-28 08:41:37 +0900

    score Text 연결

--- Fight_Cat/Assets/Scripts/Score/ScoreManager.cs
+++ Fight_Cat/Assets/Scripts/Score/ScoreManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] PlayerController _myPlayer;
+    [SerializeField] TMP_Text _scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,11 @@ public class ScoreManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SetScoreText();
+    }
+
+    private void SetScoreText()
+    {
+        _scoreText.text = _myPlayer.Score.ToString();
     }
 }
